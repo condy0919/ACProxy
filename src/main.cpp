bc86835a0c416f51d94efb9bcfadcfefda7758ee
condy0919 <condy0919@gmail.com>
2016-04-24 00:48:54 +0800

    update .gitignore

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,10 @@
 #include "log.hpp"
 
 int main() {
+    LOG_ACPROXY_FATAL("test");
     LOG_ACPROXY_ERROR("test");
+    LOG_ACPROXY_WARNING("test");
+    LOG_ACPROXY_INFO("test");
+    LOG_ACPROXY_DEBUG("test");
     return 0;
 }
